2fd593f43f532a4d8091dce4351fff31e1164f1c
Federico Farias <federico.farias@outlook.com>
2024-10-26 10:21:11 -0600

    Se incluyen decoradores por handler

--- spring-wallet-api/src/test/java/com/bintics/shared/CommandBusImplTest.java
+++ spring-wallet-api/src/test/java/com/bintics/shared/CommandBusImplTest.java
@@ -11,16 +11,42 @@ class CommandBusImplTest {
 
     @Test
     public void testExecute() {
-        var spyHandler = spy(new Demo2CommandHandler());
+        var spyHandler = spy(new Demo2Decorator2CommandHandler(new Demo2DecoratorCommandHandler(new Demo2CommandHandler())));
 
         CommandBusImpl commandBus = new CommandBusImpl();
         commandBus.add(spyHandler);
-
 
         var command = new Demo2Command("test 1");
         commandBus.execute(command);
 
         verify(spyHandler, times(1)).execute(command);
+    }
+
+    @AllArgsConstructor
+    class Demo2DecoratorCommandHandler implements CommandHandler<Demo2Command> {
+
+        private final CommandHandler<Demo2Command> cmd;
+
+        @Override
+        public void execute(Demo2Command command) {
+            System.out.println("begin transaction");
+            this.cmd.execute(command);
+            System.out.println("commit transaction");
+        }
+
+    }
+
+    @AllArgsConstructor
+    class Demo2Decorator2CommandHandler implements CommandHandler<Demo2Command> {
+
+        private final CommandHandler<Demo2Command> cmd;
+
+        @Override
+        public void execute(Demo2Command command) {
+            System.out.println("Segundo decorador");
+            this.cmd.execute(command);
+        }
+
     }
 
     class Demo2CommandHandler implements CommandHandler<Demo2Command> {
